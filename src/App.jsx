b9dec6569051993f419be15644aa6a9ad4e965b2
LaSan99 <lasannavodya@gmail.com>
2025-03-30 13:12:36 +0530

    Update jsconfig for path resolution, modify Netlify build command to disable CI, and enhance Vite configuration with output directory and sourcemaps

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
-import AdminPage from "./pages/admin/adminPage";
+import AdminPage from "./pages/admin/adminPage.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage.jsx";
-import LoginPage from "./pages/login/login";
-import RegisterPage from "./pages/register/register";
+import LoginPage from "./pages/login/login.jsx";
+import RegisterPage from "./pages/register/register.jsx";
 import { Toaster } from "react-hot-toast";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import VerifyEmail from "./pages/verifyEmail/verifyEmail";
+import VerifyEmail from "./pages/verifyEmail/verifyEmail.jsx";
 
 function App() {
   return (
